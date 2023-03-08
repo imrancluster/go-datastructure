b2ab4b329ffe5ecc44aab02767cc459b0c76a060
Imran <imrancluster@gmail.com>
2023-03-08 06:47:04 +0600

    added linkedlist example

---
+++ 02-linked-list/main.go
@@ -0,0 +1,73 @@
+package main
+
+import "fmt"
+
+type node struct {
+	data int
+	next *node
+}
+
+type linkedList struct {
+	head   *node
+	length int
+}
+
+func (l *linkedList) prepend(n *node) {
+	second := l.head
+	l.head = n
+	l.head.next = second
+	l.length++
+}
+
+func (l linkedList) printListData() {
+	toPrint := l.head
+	for l.length != 0 {
+		fmt.Printf("%d ", toPrint.data)
+		toPrint = toPrint.next
+		l.length--
+	}
+	fmt.Printf("\n")
+}
+
+func (l *linkedList) deleteWithValue(value int) {
+	if l.length == 0 {
+		return
+	}
+
+	if l.head.data == value {
+		l.head = l.head.next
+		l.length--
+		return
+	}
+
+	previousToDelete := l.head
+	for previousToDelete.next.data != value {
+		if previousToDelete.next.next == nil {
+			return
+		}
+		previousToDelete = previousToDelete.next
+	}
+	previousToDelete.next = previousToDelete.next.next
+	l.length--
+}
+
+func main() {
+	mylist := linkedList{}
+	node1 := &node{data: 100}
+	node2 := &node{data: 200}
+	node3 := &node{data: 300}
+	node4 := &node{data: 400}
+	node5 := &node{data: 500}
+	node6 := &node{data: 600}
+	mylist.prepend(node1)
+	mylist.prepend(node2)
+	mylist.prepend(node3)
+	mylist.prepend(node4)
+	mylist.prepend(node5)
+	mylist.prepend(node6)
+	mylist.printListData()
+
+	mylist.deleteWithValue(300)
+	mylist.printListData()
+
+}
